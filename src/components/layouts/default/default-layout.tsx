3d164134185cdf6de1ab42467c3ea2b4c0d89f93
richi1213 <datonanava11@gmail.com>
2025-01-01 15:08:58 +0400

    added wrapper in navbar

--- src/components/layouts/default/default-layout.tsx
+++ src/components/layouts/default/default-layout.tsx
@@ -2,8 +2,9 @@ import { Navbar } from '@/components/layouts/default/navbar';
 
 export const DefaultLayout = () => {
   return (
-    <div className='flex min-h-screen flex-col bg-background text-foreground'>
+    <div className='flex min-h-screen flex-col bg-background text-accent-foreground'>
       <Navbar />
+      {/* <ColorPalette /> */}
       {/* <Main>
         <PageContainer>
           <Outlet />
