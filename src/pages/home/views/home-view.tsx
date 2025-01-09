b28a2fdff824a5b7c48dbf08c29588e9551cbad9
richi1213 <datonanava11@gmail.com>
2025-01-09 18:15:40 +0400

    removed unused image

--- src/pages/home/views/home-view.tsx
+++ src/pages/home/views/home-view.tsx
@@ -3,10 +3,8 @@ import { lazy, Suspense } from 'react';
 
 const LazyHomePage = lazy(() => import('../components/home-page'));
 
-export const HomeView: React.FC = () => {
-  return (
-    <Suspense fallback={<Loading />}>
-      <LazyHomePage />
-    </Suspense>
-  );
-};
+export const HomeView: React.FC = () => (
+  <Suspense fallback={<Loading />}>
+    <LazyHomePage />
+  </Suspense>
+);
