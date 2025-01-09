baab4649a3cc84698021bbef33635e6b58c8949b
richi1213 <datonanava11@gmail.com>
2025-01-09 13:10:02 +0400

    added supabase

---
+++ src/supabase/index.ts
@@ -0,0 +1,12 @@
+export * from './supabase-cleint';
+export type {
+  Json,
+  Database,
+  Tables,
+  TablesInsert,
+  TablesUpdate,
+  Enums,
+  CompositeTypes,
+} from './supabase.types';
+
+export * from './api';
