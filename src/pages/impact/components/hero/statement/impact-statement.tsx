2a9b290ac3845f7ffbf87403edd1689b4ca2a857
richi1213 <datonanava11@gmail.com>
2025-01-11 20:31:51 +0400

    adjusted hero text

--- src/pages/impact/components/hero/statement/impact-statement.tsx
+++ src/pages/impact/components/hero/statement/impact-statement.tsx
@@ -4,10 +4,10 @@ export const ImpactStatement: React.FC = () => {
   return (
     <div className='flex flex-col items-center space-y-5'>
       <StatementTitle />
-      <h1 className='text-5xl font-bold tracking-wide'>
+      <h1 className='text-3xl font-bold tracking-wide sm:text-5xl'>
         National Geographic Society
       </h1>
-      <p className='text-center text-lg leading-loose'>
+      <p className='max-w-3xl text-center text-lg leading-loose'>
         We support a diverse, international community of changemakers — National
         Geographic Explorers — who use the power of science, exploration,
         education, and storytelling to illuminate and protect the wonder of our
