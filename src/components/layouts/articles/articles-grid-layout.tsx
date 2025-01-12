4c632e2641de7b49068d81f0a346262c8f0ee7d4
richi1213 <datonanava11@gmail.com>
2025-01-12 18:51:07 +0400

    added impact articles grid

--- src/components/layouts/articles/articles-grid-layout.tsx
+++ src/components/layouts/articles/articles-grid-layout.tsx
@@ -1,52 +1,12 @@
-import { ArticleCard } from '@/components';
+import { PropsWithChildren } from 'react';
 
-const staticArticles = [
-  {
-    variant: 'hero' as const,
-    category: 'Science',
-    subcategory: 'Mind, Body, Wonder',
-    title: "8 strategies to make your New Year's resolutions stick",
-    imageUrl: '/images/placeholder.svg',
-    href: '/impact',
-    isPremium: true,
-  },
-  {
-    category: 'Science',
-    title: 'The key to fitness? Strengthening your joints and tendons',
-    imageUrl: '/images/placeholder.svg',
-    href: '/impact',
-    isPremium: false,
-  },
-  {
-    category: 'Science',
-    title: 'How going sober for even a month can improve your health',
-    imageUrl: '/images/placeholder.svg',
-    href: '/impact',
-    isPremium: true,
-  },
-  {
-    category: 'Science',
-    title: 'The key to fitness? Strengthening your joints and tendons',
-    imageUrl: '/images/placeholder.svg',
-    href: '/impact',
-    isPremium: false,
-  },
-  {
-    category: 'Science',
-    title: 'How going sober for even a month can improve your health',
-    imageUrl: '/images/placeholder.svg',
-    href: '/impact',
-    isPremium: true,
-  },
-];
-
-export const ArticlesGridLayout: React.FC = () => {
+export const ArticlesGridLayout: React.FC<PropsWithChildren> = ({
+  children,
+}) => {
   return (
     <div className='mt-6'>
       <div className='grid gap-5 md:grid-cols-2 md:grid-rows-3 lg:grid-cols-3 lg:grid-rows-2'>
-        {staticArticles.map((article, index) => (
-          <ArticleCard key={index} {...article} />
-        ))}
+        {children}
       </div>
     </div>
   );
--- src/pages/impact/components/stories/articles/articles-grid/impact-articles-grid.tsx
+++ src/pages/impact/components/stories/articles/articles-grid/impact-articles-grid.tsx
@@ -1,5 +1,51 @@
-import { ArticlesGridLayout } from '@/components';
+import { ArticlesGridLayout, ArticleCard } from '@/components';
+
+const staticArticles = [
+  {
+    variant: 'hero' as const,
+    category: 'Science',
+    subcategory: 'Mind, Body, Wonder',
+    title: "8 strategies to make your New Year's resolutions stick",
+    imageUrl: '/images/placeholder.svg',
+    href: '/impact',
+    isPremium: true,
+  },
+  {
+    category: 'Science',
+    title: 'The key to fitness? Strengthening your joints and tendons',
+    imageUrl: '/images/placeholder.svg',
+    href: '/impact',
+    isPremium: false,
+  },
+  {
+    category: 'Science',
+    title: 'How going sober for even a month can improve your health',
+    imageUrl: '/images/placeholder.svg',
+    href: '/impact',
+    isPremium: true,
+  },
+  {
+    category: 'Science',
+    title: 'The key to fitness? Strengthening your joints and tendons',
+    imageUrl: '/images/placeholder.svg',
+    href: '/impact',
+    isPremium: false,
+  },
+  {
+    category: 'Science',
+    title: 'How going sober for even a month can improve your health',
+    imageUrl: '/images/placeholder.svg',
+    href: '/impact',
+    isPremium: true,
+  },
+];
 
 export const ImpactArticlesGrid: React.FC = () => {
-  return <ArticlesGridLayout />;
+  return (
+    <ArticlesGridLayout>
+      {staticArticles.map((article, index) => (
+        <ArticleCard key={index} {...article} />
+      ))}
+    </ArticlesGridLayout>
+  );
 };
