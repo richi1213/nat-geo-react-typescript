69b2521217f9b5bb50a8659bd0543cebf9eb511e
richi1213 <datonanava11@gmail.com>
2025-01-26 02:16:17 +0400

    translated

--- src/pages/home/components/tv/heading/nat-geo-tv-heading.tsx
+++ src/pages/home/components/tv/heading/nat-geo-tv-heading.tsx
@@ -1,9 +1,11 @@
 import { HeadingLine } from '@/components';
+import { useTranslation } from 'react-i18next';
 
 export const NatGeoTvHeading: React.FC = () => {
+  const { t } = useTranslation('home');
   return (
     <div className='my-36 text-center text-5xl font-bold'>
-      <h2 className='uppercase'>nat geo tv</h2>
+      <h2 className='uppercase'>{t('nat_geo_tv')}</h2>
       <HeadingLine className='mt-6 w-12' />
     </div>
   );
