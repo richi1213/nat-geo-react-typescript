1539831b4d52c3841d504dc84136503520b8464b
richi1213 <datonanava11@gmail.com>
2025-01-25 15:42:48 +0400

    translated category page

--- src/pages/category/components/header/category-header.tsx
+++ src/pages/category/components/header/category-header.tsx
@@ -1,8 +1,19 @@
 import { CategoryVideo, CategoryCover } from '@/pages';
 import type { CategoryHeaderProps } from './types';
 import { HeadingLine } from '@/components';
+import { getLocalizedString } from '@/utils';
+import { useTranslation } from 'react-i18next';
 
 export const CategoryHeader: React.FC<CategoryHeaderProps> = ({ category }) => {
+  const { i18n } = useTranslation();
+  const currentLanguage = i18n.language;
+
+  const categoryName = getLocalizedString(
+    category as Record<string, string | null>,
+    'name',
+    currentLanguage,
+  );
+
   return (
     <div className='relative h-[295px] w-full overflow-hidden'>
       {category.has_video ? (
@@ -16,7 +27,7 @@ export const CategoryHeader: React.FC<CategoryHeaderProps> = ({ category }) => {
       <div className='relative z-40 flex h-full items-center px-8 pt-28 md:px-12'>
         <h2 className='flex items-center gap-5 text-4xl font-bold uppercase tracking-wider text-foreground md:text-5xl lg:text-6xl'>
           <HeadingLine orientation='vertical' className='w-1.5' />
-          {category.name_en}
+          {categoryName}
         </h2>
       </div>
     </div>
