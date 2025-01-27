d5a7001d143a22b3402e23117f073c7b8372e9d3
richi1213 <datonanava11@gmail.com>
2025-01-28 00:04:58 +0400

    changed navbar

--- src/components/layouts/default/header/navbar/navbar.tsx
+++ src/components/layouts/default/header/navbar/navbar.tsx
@@ -3,11 +3,9 @@ import { Logos, NavbarList } from '@/components';
 export const Navbar: React.FC = () => {
   return (
     <nav className='sticky z-[100] h-[49px] w-full border-b border-gray-50 bg-foreground py-0 pl-1 pr-0 text-sm backdrop-blur-lg transition-all md:pl-2'>
-      <div className='mx-auto h-full w-full max-w-screen-xl'>
-        <div className='flex h-full items-center justify-between p-2'>
-          <Logos />
-          <NavbarList />
-        </div>
+      <div className='flex h-full justify-between p-2'>
+        <Logos />
+        <NavbarList />
       </div>
     </nav>
   );
